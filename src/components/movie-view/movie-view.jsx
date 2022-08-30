396b48bd504d5dc9a3986f6274df2cac75aa0986
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-08-30 20:21:03 +0200

    Added proptypes to movie-view component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieView extends React.Component {
   render() {
@@ -22,5 +23,26 @@ class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.instanceOf(Date).isRequired,
+      Death: PropTypes.instanceOf(Date),
+    }).isRequired,
+    Featured: PropTypes.bool.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+  }).isRequired,
+
+  onBackClick: PropTypes.func.isRequired,
+};
 
 export default MovieView;
