43ba0ac678c76307f5968d47a1d674fa0804662e
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-01 19:26:08 +0200

    installed and integrated bootstrap and styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap';
 import MainView from './components/main-view/main-view';
 
 // Import to bundle './index.scss
@@ -9,7 +10,11 @@ import './index.scss';
 // Main component
 class MovieApp extends React.Component {
   render() {
-    return (<MainView />);
+    return (
+      <Container fluid className="main-view-container" style={{ margin: 0, padding: 0 }}>
+        <MainView />
+      </Container>
+    );
   }
 }
 
