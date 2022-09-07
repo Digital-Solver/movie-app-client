21b8fbdf99b1528bc486fac86256e0adb9b34171
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-07 19:42:35 +0200

    Simplify to only show user info

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
-function UserInfo(username, email, favouriteMovies) {
-  // BUG: Props are "[object Object]" despite having been destructured
-  const getFavouriteMovies = () => {
-    if (!favouriteMovies) {
-      return <p>Favourite Some Movies To See Your List</p>;
-    }
-    return <p>{favouriteMovies}</p>;
-  };
-
+function UserInfo(username, email, birthday) {
   return (
     <div>
       <h2>Your Info:</h2>
       <p>{`Username: ${username}`}</p>
       <p>{`Email: ${email}`}</p>
-      <p>{`Favourite Movies: ${getFavouriteMovies()}`}</p>
+      <p>{`Birthday: ${birthday}`}</p>
     </div>
   );
 }
