8ce579da1956122ab85ad5da4c88938b528c60ac
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-08 21:54:38 +0200

    Create VisibilityFilterInput Component

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Form } from 'react-bootstrap';
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+  const { setFilter, visibilityFilter } = props;
+
+  return (
+    <Form.Control
+      onChange={(e) => setFilter(e.target.value)}
+      value={visibilityFilter}
+      plcaeholder="filter"
+    />
+  );
+}
+
+export default connect(null, { setFilter })(VisibilityFilterInput);
