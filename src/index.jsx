0037af1043e9aaab4ce5af1822fbdb4b6d563e3e
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-08 13:51:14 +0200

    Implement Redux Store

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Container } from 'react-bootstrap';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+
 import MainView from './components/main-view/main-view';
 
 // Import to bundle './index.scss
 import './index.scss';
 
+// Initialise Redux Store
+const store = createStore(moviesApp);
+
 // Main component
 class MovieApp extends React.Component {
   render() {
     return (
-      <Container fluid className="main-view-container" style={{ margin: 0, padding: 0 }}>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container fluid className="main-view-container" style={{ margin: 0, padding: 0 }}>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
