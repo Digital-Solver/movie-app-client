87b68f7fbf4c8f61639fb7b7cac319f27f91e0cb
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-08 13:27:39 +0200

    better commenting

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,8 @@
+// Action Types
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
+// Action Creators
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
 }
