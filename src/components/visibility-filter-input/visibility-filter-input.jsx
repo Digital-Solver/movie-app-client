0dd94657c0c0762ab6048ce30a3332515d552e1b
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-12 20:13:40 +0200

    Reafactor for readability

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-shadow */
+
+// External Dependencies
 import React from 'react';
 import { connect } from 'react-redux';
 import { Form } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+// Internal Dependencies
 import { setFilter } from '../../actions/actions';
 
+// Component
 function VisibilityFilterInput(props) {
   const { visibilityFilter, setFilter } = props;
   return (
@@ -14,4 +21,11 @@ function VisibilityFilterInput(props) {
   );
 }
 
+// PropTypes
+VisibilityFilterInput.propTypes = {
+  visibilityFilter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired,
+};
+
+// Redux & Exports
 export default connect(null, { setFilter })(VisibilityFilterInput);
