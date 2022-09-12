aaee2078f85f5321431ce21e1e8a43ef4ff7f099
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-12 14:17:14 +0200

    Transitioning login view to redux

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,10 @@
 // Action Types
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
-export const SET_USER = 'SET_USER';
+export const SET_USERDATA = 'SET_USERDATA';
+export const SET_USERNAME = 'SET_USERNAME';
+export const SET_PASSWORD = 'SET_PASSWORD';
+
 
 // Action Creators
 export function setMovies(payload) {
@@ -13,5 +16,13 @@ export function setFilter(payload) {
 }
 
 export function setUserdata(payload) {
-  return { type: SET_USER, payload };
+  return { type: SET_USERDATA, payload };
+}
+
+export function setUsername(payload) {
+  return { type: SET_USERNAME, payload };
+}
+
+export function setPassword(payload) {
+  return { type: SET_PASSWORD, payload };
 }
