b619fa4edc7a7192a352a26089dd1a7ecc62e658
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-12 21:11:49 +0200

    Refactor for readability

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prefer-stateless-function */
+
+// External Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
+
+// Internal Dependencies
 import './director-view.scss';
 
-// TODO: Make this component render - director data is being loaded from db so thats not the issue
-
+// Component
 class DirectorView extends React.Component {
+  // Class & Static Methods
   static getDeath(deathDate) {
     if (!deathDate) {
       return 'Still Alive';
@@ -14,8 +18,10 @@ class DirectorView extends React.Component {
   }
 
   render() {
+    // Props
     const { movieData, onBackClick } = this.props;
 
+    // JSX
     return (
       <Card className="director-view justify-content-md-center" style={{ backgroundColor: '#77685D', borderRadius: '5px' }}>
         <Card.Title style={{ color: 'white' }} className="director-name">{movieData.Director.Name}</Card.Title>
@@ -27,6 +33,7 @@ class DirectorView extends React.Component {
   }
 }
 
+// PropTypes
 DirectorView.propTypes = {
   movieData: PropTypes.shape({
     Title: PropTypes.string.isRequired,
@@ -47,4 +54,5 @@ DirectorView.propTypes = {
   onBackClick: PropTypes.func.isRequired,
 };
 
+// Export
 export default DirectorView;
