62e074ccaf99c7fa64d65b5b7653d7898f115870
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-12 19:09:19 +0200

    Clean up unused imports

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-alert */
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
