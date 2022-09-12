1e0da03bd477f50ae27b72287a1f316304b7391f
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-12 19:06:40 +0200

    Transitioned user update component to use redux

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,35 +1,35 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-alert */
 import React, { useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  setUsername, setPassword, setEmail, setBirthday,
+} from '../../actions/actions';
 
-export default function UpdateUser(props) {
+function UpdateUser(props) {
   const {
-    token, username, email, birth, password,
+    userdata, setUsername, setPassword, setEmail, setBirthday,
   } = props;
-
-  const [newUsername, setNewUsername] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [newEmail, setNewEmail] = useState('');
-  const [newBirthday, setNewBirthday] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.put(
-      `https://kds-movie-api.herokuapp.com/users/${username}`,
+      `https://kds-movie-api.herokuapp.com/users/${localStorage.getItem('user')}`,
       {
-        Username: newUsername || username,
-        Email: newEmail || email,
-        Password: newPassword,
-        Birthday: newBirthday || birth,
+        Username: userdata.user.Username,
+        Email: userdata.user.Email,
+        Password: userdata.user.Password,
+        Birthday: userdata.user.Birthday,
       },
-      { headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' } },
+      { headers: { Authorization: `Bearer ${userdata.token}`, 'Content-Type': 'application/json' } },
     ).then((res) => {
-      alert(`${username}'s profile has been updated.`);
-      localStorage.setItem('user', newUsername || username);
-      window.open(`/users/${newUsername || username}}`, '_self');
+      alert(`${userdata.user.Username}'s profile has been updated.`);
+      localStorage.setItem('user', userdata.user.Username);
+      window.open(`/users/${userdata.user.Username}}`, '_self');
     }).catch((err) => console.log(err));
   };
 
@@ -39,22 +39,22 @@ export default function UpdateUser(props) {
 
       <label>
         Username:
-        <input type="text" name="Username" defaultValue={username} onChange={(e) => setNewUsername(e.target.value)} />
+        <input type="text" name="Username" defaultValue={userdata.user.Username} onChange={(e) => setUsername(e.target.value)} />
       </label>
 
       <label>
         Email:
-        <input type="email" name="Email" defaultValue={email} onChange={(e) => setNewEmail(e.target.value)} />
+        <input type="email" name="Email" defaultValue={userdata.user.Email} onChange={(e) => setEmail(e.target.value)} />
       </label>
 
       <label>
         Password:
-        <input type="password" name="Password" required defaultValue="" onChange={(e) => setNewPassword(e.target.value)} />
+        <input type="password" name="Password" required defaultValue="" onChange={(e) => setPassword(e.target.value)} />
       </label>
 
       <label>
         Date of Birth:
-        <input type="date" name="DoB" defaultValue={birth} onChange={(e) => setNewBirthday(e.target.value)} />
+        <input type="date" name="DoB" defaultValue={userdata.user.Birthday} onChange={(e) => setBirthday(e.target.value)} />
       </label>
 
       <button type="submit">Submit</button>
@@ -64,9 +64,26 @@ export default function UpdateUser(props) {
 }
 
 UpdateUser.propTypes = {
-  username: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  birth: PropTypes.string.isRequired,
-  token: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
+  userdata: PropTypes.shape({
+    user: PropTypes.shape({
+      Username: PropTypes.string.isRequired,
+      Password: PropTypes.string,
+      Email: PropTypes.string.isRequired,
+      Birthday: PropTypes.string.isRequired,
+    }),
+    token: PropTypes.string.isRequired,
+  }).isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  setBirthday: PropTypes.func.isRequired,
 };
+
+function mapStateToProps(state) {
+  const { userdata } = state;
+  return { userdata };
+}
+
+export default connect(mapStateToProps, {
+  setUsername, setPassword, setEmail, setBirthday,
+})(UpdateUser);
