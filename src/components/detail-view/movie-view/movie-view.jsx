379cd34624757d9a14a0ce84a2ce38428e2decbb
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-09-14 14:09:30 +0200

    Refactored detail views, scss, & file structure

--- src/components/detail-view/movie-view/movie-view.jsx
+++ src/components/detail-view/movie-view/movie-view.jsx
@@ -3,76 +3,74 @@
 // External Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {
   Button, Card, Row, Col,
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 // Internal Dependencies
-import './director-view.scss';
+import '../detail-view.scss';
 
 // Component
-class DirectorView extends React.Component {
-  // Class & Static Methods
-  static getDeath(deathDate) {
-    if (!deathDate) {
-      return 'Still Alive';
-    } return deathDate;
-  }
-
+class MovieView extends React.Component {
+  // Class Methods
   render() {
-    // Props
     const { movieData, onBackClick } = this.props;
     const posterAlt = `${movieData.Title} Poster`;
 
     // JSX
     return (
-      <Card className="director-view justify-content-md-center">
-        <Row className="movie-view-row">
-          <Col className="movie-view-poster-container" lg={12} xl={6}>
+      <Card className="detail-view justify-content-md-center">
+        <Row className="detail-view-row">
+          <Col className="detail-view-poster-container" lg={12} xl={6}>
             <Card.Img variation="top" className="movie-poster" src={movieData.ImageURL} crossOrigin="anonymous" alt={posterAlt} />
           </Col>
 
-          <Col lg={12} xl={6} className="movie-view-content-outer-container">
-            <div className="movie-view-content-inner-container">
+          <Col lg={12} xl={6} className="detail-view-content-outer-container">
+            <div className="detail-view-content-inner-container">
               <Card.Title className="movie-title">{movieData.Title}</Card.Title>
               <div className="detail-navigation">
                 <Link to={`/movies/${movieData._id}`}>
-                  <Button variant="movie-detail" className="movie-detail-button" type="button">Scynopsis</Button>
+                  <Button variant="movie-active-detail" className="movie-detail-button" type="button">Scynopsis</Button>
                 </Link>
                 <Link to={`/directors/${movieData.Director.Name}`}>
-                  <Button variant="movie-active-detail" className="movie-detail-button" type="button">Director</Button>
+                  <Button variant="movie-detail" className="movie-detail-button" type="button">Director</Button>
                 </Link>
                 <Link to={`/genres/${movieData.Genre.Name}`}>
                   <Button type="button" variant="movie-detail">Genre</Button>
                 </Link>
               </div>
-              <Card.Body className="director-name h5">{movieData.Director.Name}</Card.Body>
-              <Card.Body className="director-life">{`${movieData.Director.Birth} - ${DirectorView.getDeath(movieData.Director.Death)}`}</Card.Body>
-              <Card.Body className="director-bio">{movieData.Director.Bio}</Card.Body>
+              <Card.Body className="movie-description">{movieData.Description}</Card.Body>
               <Button type="button" onClick={() => { onBackClick(); }}>Back</Button>
             </div>
           </Col>
         </Row>
       </Card>
-
     );
   }
 }
 
 // PropTypes
-DirectorView.propTypes = {
+MovieView.propTypes = {
   movieData: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
+      Birth: PropTypes.string,
       Death: PropTypes.string,
     }).isRequired,
+    Featured: PropTypes.bool.isRequired,
     ImageURL: PropTypes.string.isRequired,
   }).isRequired,
   onBackClick: PropTypes.func.isRequired,
 };
 
 // Export
-export default DirectorView;
+export default MovieView;
